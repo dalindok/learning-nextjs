69765eddff95100ec85be22218424d3e945715ba
dalindok <dalindok211@gmail.com>
2024-12-24 20:48:58 +0700

    init project

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const customers = () => {
+  return <div>Customer Page</div>;
+};
+
+export default customers;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const invoice = () => {
+  return <div>Invoice page</div>;
+};
+
+export default invoice;
